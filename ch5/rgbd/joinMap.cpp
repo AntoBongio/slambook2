508e0e5f55ea67fc3bd689f3b2e429f2420879e8
Antonino Bongiovanni <antoniobongio@gmail.com>
2024-05-16 22:56:49 +0200

    Ch5 Checked.

--- ch5/rgbd/joinMap.cpp
+++ ch5/rgbd/joinMap.cpp
@@ -18,16 +18,16 @@ int main(int argc, char **argv) {
     vector<cv::Mat> colorImgs, depthImgs;    // 彩色图和深度图
     TrajectoryType poses;         // 相机位姿
 
-    ifstream fin("./pose.txt");
+    ifstream fin("/home/antonino/Desktop/slam/slambook2/ch5/rgbd/pose.txt");
     if (!fin) {
-        cerr << "请在有pose.txt的目录下运行此程序" << endl;
+        cerr << "Please run the program in the directory that has pose.txt" << endl;
         return 1;
     }
 
     for (int i = 0; i < 5; i++) {
-        boost::format fmt("./%s/%d.%s"); //图像文件格式
+        boost::format fmt("/home/antonino/Desktop/slam/slambook2/ch5/rgbd/%s/%d.%s"); //image format
         colorImgs.push_back(cv::imread((fmt % "color" % (i + 1) % "png").str()));
-        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
+        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1)); // use -1 flag to load depth image
 
         double data[7] = {0};
         for (auto &d:data)
@@ -37,8 +37,7 @@ int main(int argc, char **argv) {
         poses.push_back(pose);
     }
 
-    // 计算点云并拼接
-    // 相机内参 
+    // compute the point cloud using camera intrinsics 
     double cx = 325.5;
     double cy = 253.5;
     double fx = 518.0;
@@ -48,14 +47,14 @@ int main(int argc, char **argv) {
     pointcloud.reserve(1000000);
 
     for (int i = 0; i < 5; i++) {
-        cout << "转换图像中: " << i + 1 << endl;
+        cout << "Converting RGBD images " << i + 1 << endl;
         cv::Mat color = colorImgs[i];
         cv::Mat depth = depthImgs[i];
         Sophus::SE3d T = poses[i];
         for (int v = 0; v < color.rows; v++)
             for (int u = 0; u < color.cols; u++) {
-                unsigned int d = depth.ptr<unsigned short>(v)[u]; // 深度值
-                if (d == 0) continue; // 为0表示没有测量到
+                unsigned int d = depth.ptr<unsigned short>(v)[u]; // depth value is 16 bit
+                if (d == 0) continue; // 0 means no valida value
                 Eigen::Vector3d point;
                 point[2] = double(d) / depthScale;
                 point[0] = (u - cx) * point[2] / fx;
@@ -71,7 +70,7 @@ int main(int argc, char **argv) {
             }
     }
 
-    cout << "点云共有" << pointcloud.size() << "个点." << endl;
+    cout << "global point cloud has " << pointcloud.size() << " points." << endl;
     showPointCloud(pointcloud);
     return 0;
 }
--- ch5/stereo/stereoVision.cpp
+++ ch5/stereo/stereoVision.cpp
@@ -9,8 +9,8 @@ using namespace std;
 using namespace Eigen;
 
 // 文件路径
-string left_file = "./left.png";
-string right_file = "./right.png";
+string left_file = "/home/antonino/Desktop/slam/slambook2/ch5/stereo/left.png";
+string right_file = "/home/antonino/Desktop/slam/slambook2/ch5/stereo/right.png";
 
 // 在pangolin中画图，已写好，无需调整
 void showPointCloud(
@@ -18,31 +18,30 @@ void showPointCloud(
 
 int main(int argc, char **argv) {
 
-    // 内参
+    // Intrinsic
     double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
-    // 基线
+    // Baseline
     double b = 0.573;
 
-    // 读取图像
     cv::Mat left = cv::imread(left_file, 0);
     cv::Mat right = cv::imread(right_file, 0);
     cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
-        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的参数
+        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // SGBM is sensitive to params
     cv::Mat disparity_sgbm, disparity;
     sgbm->compute(left, right, disparity_sgbm);
     disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
 
-    // 生成点云
+    // Compute the point cloud
     vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
 
-    // 如果你的机器慢，请把后面的v++和u++改成v+=2, u+=2
     for (int v = 0; v < left.rows; v++)
         for (int u = 0; u < left.cols; u++) {
             if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
 
-            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); // 前三维为xyz,第四维为颜色
+            // thefirst 3 dimensions are xyz, the last one is the colour
+            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); 
 
-            // 根据双目模型计算 point 的位置
+            // Compute the depth from disparity
             double x = (u - cx) / fx;
             double y = (v - cy) / fy;
             double depth = fx * b / (disparity.at<float>(v, u));
@@ -55,7 +54,7 @@ int main(int argc, char **argv) {
 
     cv::imshow("disparity", disparity / 96.0);
     cv::waitKey(0);
-    // 画出点云
+    // show point cloud in pangolin
     showPointCloud(pointcloud);
     return 0;
 }
