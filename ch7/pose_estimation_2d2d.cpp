4122548a7613b9d94f87dfa87c7594aa8ecaad7b
Antonino Bongiovanni <antoniobongio@gmail.com>
2024-06-13 00:22:33 +0200

    Pose estimation 2d2d and traingulation. Next step: Merge them to recreate a simple SLAM pipeline.

--- ch7/pose_estimation_2d2d.cpp
+++ ch7/pose_estimation_2d2d.cpp
@@ -9,7 +9,7 @@ using namespace std;
 using namespace cv;
 
 /****************************************************
- * 本程序演示了如何使用2D-2D的特征匹配估计相机运动
+ * Pose estimation with 2D-2D correspondences
  * **************************************************/
 
 void find_feature_matches(
@@ -24,37 +24,45 @@ void pose_estimation_2d2d(
   std::vector<DMatch> matches,
   Mat &R, Mat &t);
 
-// 像素坐标转相机归一化坐标
 Point2d pixel2cam(const Point2d &p, const Mat &K);
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    cout << "usage: pose_estimation_2d2d img1 img2" << endl;
-    return 1;
-  }
-  //-- 读取图像
-  Mat img_1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
+   
+  std::string img1_path = "/home/antonino/Desktop/slam/slambook2/ch7/1.png";
+  std::string img2_path = "/home/antonino/Desktop/slam/slambook2/ch7/2.png";
+
+  Mat img_1 = imread(img1_path, cv::IMREAD_COLOR);
+  Mat img_2 = imread(img2_path, cv::IMREAD_COLOR);
   assert(img_1.data && img_2.data && "Can not load images!");
 
   vector<KeyPoint> keypoints_1, keypoints_2;
   vector<DMatch> matches;
   find_feature_matches(img_1, img_2, keypoints_1, keypoints_2, matches);
-  cout << "一共找到了" << matches.size() << "组匹配点" << endl;
+  cout << "In total we get " << matches.size() << " set of feature points." << endl;
 
-  //-- 估计两张图像间运动
+  //-- Estimate the motion between two frames
   Mat R, t;
   pose_estimation_2d2d(keypoints_1, keypoints_2, matches, R, t);
 
-  //-- 验证E=t^R*scale
+  //--  Check E = t^R*scale
   Mat t_x =
     (Mat_<double>(3, 3) << 0, -t.at<double>(2, 0), t.at<double>(1, 0),
       t.at<double>(2, 0), 0, -t.at<double>(0, 0),
       -t.at<double>(1, 0), t.at<double>(0, 0), 0);
 
+  // In theory, for correct R and t, the product t^*R should relate to the essential matrix.
   cout << "t^R=" << endl << t_x * R << endl;
 
-  //-- 验证对极约束
+  //-- Check epipolar constraints
+  /**
+   * For each match, the corresponding points in the camera coordinate system are computed 
+   * using the pixel2cam function.
+   * 
+   * Points are converted to homogeneous coordinates (y1 and y2).
+   * The epipolar constraint y2^T * t_x * R * y1 is computed for each pair and printed. 
+   * Ideally, this value should be close to zero for inliers, indicating that the points
+   *  satisfy the epipolar constraint.
+   */
   Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
   for (DMatch m: matches) {
     Point2d pt1 = pixel2cam(keypoints_1[m.queryIdx].pt, K);
@@ -64,6 +72,8 @@ int main(int argc, char **argv) {
     Mat d = y2.t() * t_x * R * y1;
     cout << "epipolar constraint = " << d << endl;
   }
+
+
   return 0;
 }
 
@@ -71,7 +81,7 @@ void find_feature_matches(const Mat &img_1, const Mat &img_2,
                           std::vector<KeyPoint> &keypoints_1,
                           std::vector<KeyPoint> &keypoints_2,
                           std::vector<DMatch> &matches) {
-  //-- 初始化
+
   Mat descriptors_1, descriptors_2;
   // used in OpenCV3
   Ptr<FeatureDetector> detector = ORB::create();
@@ -80,23 +90,19 @@ void find_feature_matches(const Mat &img_1, const Mat &img_2,
   // Ptr<FeatureDetector> detector = FeatureDetector::create ( "ORB" );
   // Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ( "ORB" );
   Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-  //-- 第一步:检测 Oriented FAST 角点位置
+
   detector->detect(img_1, keypoints_1);
   detector->detect(img_2, keypoints_2);
 
-  //-- 第二步:根据角点位置计算 BRIEF 描述子
   descriptor->compute(img_1, keypoints_1, descriptors_1);
   descriptor->compute(img_2, keypoints_2, descriptors_2);
 
-  //-- 第三步:对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
   vector<DMatch> match;
   //BFMatcher matcher ( NORM_HAMMING );
   matcher->match(descriptors_1, descriptors_2, match);
 
-  //-- 第四步:匹配点对筛选
   double min_dist = 10000, max_dist = 0;
 
-  //找出所有匹配之间的最小距离和最大距离, 即是最相似的和最不相似的两组点之间的距离
   for (int i = 0; i < descriptors_1.rows; i++) {
     double dist = match[i].distance;
     if (dist < min_dist) min_dist = dist;
@@ -106,7 +112,6 @@ void find_feature_matches(const Mat &img_1, const Mat &img_2,
   printf("-- Max dist : %f \n", max_dist);
   printf("-- Min dist : %f \n", min_dist);
 
-  //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
   for (int i = 0; i < descriptors_1.rows; i++) {
     if (match[i].distance <= max(2 * min_dist, 30.0)) {
       matches.push_back(match[i]);
@@ -126,40 +131,65 @@ void pose_estimation_2d2d(std::vector<KeyPoint> keypoints_1,
                           std::vector<KeyPoint> keypoints_2,
                           std::vector<DMatch> matches,
                           Mat &R, Mat &t) {
-  // 相机内参,TUM Freiburg2
+
+  // Camera Intrinsics,TUM Freiburg2
   Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
 
-  //-- 把匹配点转换为vector<Point2f>的形式
+  //-- Convert the matching point to the form of vector <Point2f>
   vector<Point2f> points1;
   vector<Point2f> points2;
 
+  // matches[i].queryIdx: Index of Kp in first image
+  // matches[i].trainIdx: Index of Kp in second image
   for (int i = 0; i < (int) matches.size(); i++) {
     points1.push_back(keypoints_1[matches[i].queryIdx].pt);
     points2.push_back(keypoints_2[matches[i].trainIdx].pt);
   }
 
-  //-- 计算基础矩阵
+  //-- Calculate fundamental matrix: F
+  /**
+   * Detailed Explanation of Methods
+      7-point algorithm (FM_7POINT):
+        Requires exactly 7 points.
+        It computes up to 3 solutions for the fundamental matrix and then validates them.
+
+      8-point algorithm (FM_8POINT):
+        Requires at least 8 points.
+        Solves a linear equation system to estimate the fundamental matrix.
+        Typically used in well-conditioned scenarios where there is sufficient data and minimal noise.
+      
+      RANSAC (FM_RANSAC):
+
+        Robust method that iteratively estimates the fundamental matrix while discarding outliers.
+        Suitable for real-world data with noise and outliers.
+        Requires two additional parameters: the reprojection error threshold (param1) and the confidence level (param2).
+
+      LMedS (FM_LMEDS):
+        Another robust method that minimizes the median of the reprojection errors.
+        Also handles outliers effectively.
+  */
   Mat fundamental_matrix;
-  fundamental_matrix = findFundamentalMat(points1, points2, CV_FM_8POINT);
+  fundamental_matrix = findFundamentalMat(points1, points2, FM_8POINT);
   cout << "fundamental_matrix is " << endl << fundamental_matrix << endl;
 
-  //-- 计算本质矩阵
-  Point2d principal_point(325.1, 249.7);  //相机光心, TUM dataset标定值
-  double focal_length = 521;      //相机焦距, TUM dataset标定值
+  //-- Calculate essential matrix: E
+  Point2d principal_point(325.1, 249.7);  // (cx, cy)
+  double focal_length = 521;      // Passing these two values is like passing K
   Mat essential_matrix;
   essential_matrix = findEssentialMat(points1, points2, focal_length, principal_point);
   cout << "essential_matrix is " << endl << essential_matrix << endl;
 
-  //-- 计算单应矩阵
-  //-- 但是本例中场景不是平面，单应矩阵意义不大
+  //-- Calculate Homography: H
+  //  to find the homography matrix that relates two planes in different images
+  //-- But the scene is not planar，and calculating the homography here is of little significance
   Mat homography_matrix;
   homography_matrix = findHomography(points1, points2, RANSAC, 3);
   cout << "homography_matrix is " << endl << homography_matrix << endl;
 
-  //-- 从本质矩阵中恢复旋转和平移信息.
-  // 此函数仅在Opencv3中提供
+  //-- Recover rotation and translation from E
   recoverPose(essential_matrix, points1, points2, R, t, focal_length, principal_point);
   cout << "R is " << endl << R << endl;
   cout << "t is " << endl << t << endl;
 
 }
+
